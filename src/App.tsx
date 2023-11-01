031a0526e61be83fe5358e9c49bbdc5a5497ccb8
Lifanovaa25 <50072172+Lifanovaa25@users.noreply.github.com>
2023-11-01 14:33:51 +0300

    pizza

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React, { FC, useState } from "react";
+import "./App.css";
+import AddPizzaForm from "./components/AddPizzaForm";
+import Pizza from "./modals/Pizza";
+
+const App = () => {
+  const [pizzasList, setPizzasList] = useState<Pizza[]>([]);
+  const addPizza = (newPizza: Pizza) => {
+    setPizzasList([...pizzasList, newPizza]);
+  };
+  console.log(pizzasList);
+  return (
+    <div className="App">
+      <div className="wrap">
+        <span className="heading">Наша пиццерия</span>
+        <AddPizzaForm addPizza={addPizza} />
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/AddPizzaForm.tsx
@@ -0,0 +1,74 @@
+import React, { FC, ChangeEvent, FormEvent, useState } from "react";
+import "./styles.css";
+import Pizza from "../modals/Pizza";
+
+interface AddPizzaProps {
+  addPizza: (newPizza: Pizza) => void;
+}
+
+const initState = {
+  title: "",
+  price: "",
+  img: "",
+};
+
+const AddPizzaForm: FC<AddPizzaProps> = ({ addPizza }) => {
+  const [newPizza, setNewPizza] = useState<{
+    title: string;
+    price: string;
+    img: string;
+  }>(initState);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    // console.log("name >>", name);
+    // console.log("value >>", value);
+    setNewPizza({
+      ...newPizza,
+      [name]: value,
+    });
+  };
+  console.log("new pizza>>", newPizza);
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const { title, price, img } = newPizza;
+    if (title && price && img) {
+      addPizza({
+        title,
+        img,
+        price: Number(price),
+        id: Date.now(),
+      });
+      setNewPizza(initState);
+    }
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        name="title"
+        type="text"
+        placeholder="Название"
+        onChange={handleChange}
+        value={newPizza.title}
+      />
+      <input
+        name="price"
+        type="text"
+        placeholder="Стоимость"
+        onChange={handleChange}
+        value={newPizza.price}
+      />
+      <input
+        name="img"
+        type="text"
+        placeholder="Изображение"
+        onChange={handleChange}
+        value={newPizza.img}
+      />
+      <button type="submit">Добавить в меню</button>
+    </form>
+  );
+};
+
+export default AddPizzaForm;
