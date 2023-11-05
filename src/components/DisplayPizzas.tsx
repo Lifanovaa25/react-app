8b84153f464c30e701ac679f04e8b9e42d800692
Lifanovaa25 <50072172+Lifanovaa25@users.noreply.github.com>
2023-11-05 13:51:15 +0300

    CSSTransition

--- src/components/DisplayPizzas.tsx
+++ src/components/DisplayPizzas.tsx
@@ -1,15 +1,21 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import Pizza from "../modals/Pizza";
-import SinglePizza from "../components/SinglePizza"
+import SinglePizza from "../components/SinglePizza";
+import { CSSTransition } from "react-transition-group";
 
 interface DisplayPizzasProps {
   pizzasList: Pizza[];
 }
 const DisplayPizzas: FC<DisplayPizzasProps> = ({ pizzasList }) => {
+  const [show, setShow] = useState(false);
+
   return (
     <div className="container">
       {pizzasList.map((pizza) => {
-        return <SinglePizza  key={pizza.id} pizza={pizza} />
+        return (
+        <CSSTransition in={true} timeout={500} unmountOnExit>
+        <SinglePizza key={pizza.id} pizza={pizza} />
+        </CSSTransition>);
       })}
     </div>
   );
